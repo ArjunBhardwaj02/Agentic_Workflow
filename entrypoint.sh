0d9a40670aaa9ec3317a7949a1389ce71740af68
Arjun Bhardwaj <arjunbhardwaj0274@gmail.com>
2026-06-27 17:16:02 +0530

    fix: simplify entrypoint, use secret manager volume

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-mkdir -p /app/.streamlit
-
-python3 -c "
-import os, sys
-content = os.environ.get('STREAMLIT_SECRETS_TOML', '')
-if content:
-    with open('/app/.streamlit/secrets.toml', 'w') as f:
-        f.write(content)
-    print('secrets.toml written OK', flush=True)
-else:
-    print('WARNING: STREAMLIT_SECRETS_TOML is empty', flush=True)
-"
-
 exec streamlit run frontend.py \
     --server.port ${PORT:-8080} \
     --server.address 0.0.0.0 \
